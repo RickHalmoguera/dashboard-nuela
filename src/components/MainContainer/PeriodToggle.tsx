ccb4a490c1b82f07ee425f5e824a2749a25889e1
Ricardo Halmoguera <halmoguerar@gmail.com>
2024-03-29 18:02:57 +0100

    ADD: HOURS COMPONENT DONE

--- src/components/MainContainer/PeriodToggle.tsx
+++ src/components/MainContainer/PeriodToggle.tsx
@@ -1,7 +1,7 @@
 
 export default function PeriodToggle(){
     return(
-        <div className="w-[238px] h-12 bg-toggle-gray flex justify-center items-center rounded-lg">
+        <div className="w-[238px] h-12 bg-toggle-gray flex justify-center items-center rounded-lg mb-8">
             <label className="flex items-center p-2 rounded-md cursor-pointer text-font-black text-center">
                 <input type="checkbox" className="hidden peer" />
                 <span className="px-4 py-2 w-28 rounded-l-md bg-white peer-checked:bg-toggle-gray">Semanal</span>
